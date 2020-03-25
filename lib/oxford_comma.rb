e90d721b9bf1381fe242f3fcf72858e845472c16
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-25 23:13:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.count == 2 
     return array.join(" and ")
   else
-    array[-1] = "and #{array -1}"
-    return array[-1].join(" , ")
+    array[-1] = "and #{array [-1]}"
+    return array.join(" , ")
   end
 end
