7231d9d122320502a32bc22c47a3c33530b91175
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-25 23:10:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.count == 1 
-    return array.split
+    return array.join
   elsif array.count == 2 
-    return array.split("and")
+    return array.join("and")
   else
     array[-1] = "and #{array -1}"
-    return array.split(",")
+    return array.join(",")
   end
 end
